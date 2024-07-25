d813ff5cca2de4bb3f297690215b151be2347d87
kathyslate <mercyoyetunde98@gmail.com>
2024-07-25 12:17:09 -0700

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,17 @@
-#!/usr/bin/env python3
-from typing import List
+#!/usr/bin/python3
+"""Pascal's Triangle"""
 
 
-def pascal_triangle(n: int) -> List[list]:
-    '''
-    class Pascal triangle
-    '''
-    if n <= 0:
-        return []
-
-    if n == 1:
-        return [[1]]
-
-    if n == 2:
-        return [[1], [1, 1]]
-
-    triangle = [[1], [1, 1]]
-
-    for i in range(2, n):
-        temp = [1, 1]
-        for j in range(0, len(triangle[-1])-1):
-            a = triangle[-1][j]
-            b = triangle[-1][j+1]
-            temp.insert(-1, a + b)
-        triangle.append(temp)
-
-    return triangle
+def pascal_triangle(n):
+    """Create a function def pascal_triangle(n): returns a list of lists
+    """
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
